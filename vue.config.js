ae86a1bc62318bd7e3ee17116f8e5a4a31bc5e19
yancy <yanguang@wudsec.com>
2020-10-13 17:29:26 +0800

    1

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 const webpack = require('webpack')
 module.exports = {
     publicPath: process.env.NODE_ENV === 'production'
-        ? '/temp-cli/'
+        ? '/tef-cli/'
         : '/',
     outputDir: 'dist',
     devServer: {
