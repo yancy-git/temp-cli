fb5969a58c70cb3ce5c6ca71985e6b0349f5d011
yancy-git <59213409+yancy-git@users.noreply.github.com>
2020-10-13 17:31:25 +0800

    白屏修复

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 const webpack = require('webpack')
 module.exports = {
     publicPath: process.env.NODE_ENV === 'production'
-        ? '/tef-cli/'
+        ? '/temp-cli/'
         : '/',
     outputDir: 'dist',
     devServer: {
